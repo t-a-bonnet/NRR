75b89344e1107cee2a90c132a5b5ca9f5c7f7eb7
tbonnet <engs2670@engs-30935.local>
2024-08-27 22:09:19 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nrr',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pyterrier',
+        'textdistance',
+        'torch',
+        'fuzzywuzzy',
+        'nltk',
+        'scikit-learn',
+        'unidecode'
+    ],
+    description='A library for Neural Retrieval Refinement (NRR)',
+    author='Your Name',
+    author_email='bonnet.tyler.alexander@gmail.com',
+    url='https://github.com/t-a-bonnet/NRR',  # Replace with your repository URL
+)
